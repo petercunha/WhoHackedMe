358fc6601f0f683fe7fd9ce078290c18df11d161
Peter Cunha <petercunha8@gmail.com>
2018-10-10 02:28:23 -0400

    Edit favicon

--- components/head.js
+++ components/head.js
@@ -19,7 +19,7 @@ const Head = props => (
     <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
     <link rel="apple-touch-icon" href="/static/touch-icon.png" />
     <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
-    <link rel="icon" href="/static/favicon.ico" />
+    <link rel="icon" href="/static/favicon.png" />
     <meta property="og:url" content={props.url || defaultOGURL} />
     <meta property="og:title" content={props.title || ''} />
     <meta
